b7650a3945a73683e7ab582345acb00577521ab2
FANGPLAY SIRIRAK <fangplay_2014@hotmail.com>
2023-08-02 17:13:33 +0700

    Basic Info Update

---
+++ src/components/Profile.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div class="letter">
+        <h1 class="green">{{ msg }}</h1>
+        <h2 class="blue">FANGPLAY Official Website</h2>
+        <h4 class="red">FANGPLAY Sirirak Sophakarn</h4>
+    </div>
+    
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 400;
+  font-size: 2.5rem;
+  position: relative;
+  top: -5px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.letter h1,
+.letter h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .letter h1,
+  .letter h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheProfile.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ProfileItem from './ProfileItem.vue'
+</script>
+
+<template>
+    <ProfileItem>
+        <template #heading>Birth Date</template>
+        <p>5 August 1995</p>
+    </ProfileItem>
+    <ProfileItem>
+        <template #heading>Langauge</template>
+        <p>Thai,English,Chinese,Japanese,Korean</p>
+    </ProfileItem>
+    <ProfileItem>
+        <template #heading>Education</template>
+        <h4>Bechalor's Degree</h4>
+        <p>B.Sc.Computer Science Thaksin University</p>
+    </ProfileItem>
+</template>
