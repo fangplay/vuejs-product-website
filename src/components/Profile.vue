aa6e499aae3b1b5cbbd9b0e53093fb08209b0dce
FANGPLAY SIRIRAK <40180085+fangplay@users.noreply.github.com>
2023-08-02 21:08:50 +0700

    update content

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -13,9 +13,15 @@ defineProps({
         <h2 class="blue">FANGPLAY Official Website</h2>
         <h4 class="red">FANGPLAY Sirirak Sophakarn</h4>
     </div>
-    <div class="slogan">
+    <div class="starter">
         <h1>Basic Information</h1>
         <p>Hello everyone, Welcome to FANGPLAY official website.<br>Now this site have available now!</p>
+    </div>
+    <div class="slogan">
+      <p>"Though I'm alone but not alone all times."</p>
+      <p>"雖然我一個人，但並非總是孤單。"</p>
+      <p>"私は一人ですが、すべての時間だけではありません。"</p>
+      <p>"나는 혼자이지만 항상 혼자가 아닙니다."</p>
     </div>
     
 </template>
@@ -35,6 +41,14 @@ h3 {
 .letter h1,
 .letter h3 {
   text-align: center;
+}
+
+.slogan h1,h2,h3,h4,h5{
+  font-size: 1.5rem;
+}
+
+.starter h1,h2,h3,h4,h5{
+  font-size: 2.5rem;
 }
 
 @media (min-width: 1024px) {
--- src/components/TheProfile.vue
+++ src/components/TheProfile.vue
@@ -16,4 +16,13 @@ import ProfileItem from './ProfileItem.vue'
         <h4>Bechalor's Degree</h4>
         <p>B.Sc.Computer Science Thaksin University</p>
     </ProfileItem>
+    <ProfileItem>
+        <template #heading>Skills</template>
+        <h3>Programming</h3>
+        <p>PHP,JavaScript,C#,Python,HTML,CSS,Java,Kotlin,ReactJS,VueJS,Laravel,FastAPI,DJango,.Net,ASP.Net</p>
+        <h3>Operating System</h3>
+        <p>Linux,Windows</p>
+        <h3>Multimedia</h3>
+        <p>Adobe Photoshop,Adobe Illustrator,Adobe Premire Pro,Adobe After Effects,Adobe Audition,VegasPro</p>
+    </ProfileItem>
 </template>
